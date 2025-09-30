b42d9e297255b3f36bd6470b3b31901865cf9178
tanvirabsar <tjoy4799@gmail.com>
2025-09-30 16:34:26 +0600

    setup db and add different file for routing

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const { run } = require('./db')
+const { router } = require('./app')
 const app = express()
 const port = 8080
+
+run()
+
+app.use(express.json())
+
 
 app.get('/',(req,res)=>{
     res.send({message: 'data is cooking'})
 })
 
+app.use('/api', router)
 app.listen(port,()=>{
     console.log(`server is runing on http://localhost:${port}`);
 })
 
+module.exports = {app}
